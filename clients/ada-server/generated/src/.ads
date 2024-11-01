d37358bb2b103f9a276fe0e2f9039960dc04c311
pakkunbot <blah+pakkun@cliffano.com>
2024-11-01 11:48:23 +0000

    Updates

---
+++ clients/ada-server/generated/src/.ads
@@ -0,0 +1,14 @@
+--  OpenAPI Okta IP Ranges
+--  OpenAPI specification and a set of generated API clients for Okta IP Ranges
+--  ------------ EDIT NOTE ------------
+--  This file was generated with openapi-generator.  You can modify it to implement
+--  the server.  After you modify this file, you should add the following line
+--  to the .openapi-generator-ignore file:
+--
+--  src/.ads
+--
+--  Then, you can drop this edit note comment.
+--  ------------ EDIT NOTE ------------
+package  is
+
+end ;
